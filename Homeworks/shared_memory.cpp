bcd714528605af513e00fc3ab58bd12f5482db32
Hrips03 <hripsimebarsegyan2003@gmail.com>
2025-03-22 11:08:17 +0400

    shared_memory

---
+++ Homeworks/shared_memory.cpp
@@ -0,0 +1,35 @@
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <iostream>
+
+int main()
+{
+    int *shared_var = (int *)mmap(NULL, sizeof(int),
+                                  PROT_READ | PROT_WRITE,
+                                  MAP_SHARED | MAP_ANONYMOUS,
+                                  -1, 0);
+
+    if (shared_var == MAP_FAILED)
+        perror("mmap");
+
+    pid_t pid = fork();
+
+    if (pid == -1)
+        perror("fork");
+    else if (pid == 0)
+    {
+        std::cout << "Child process's shared_var = " << *shared_var << "\n";
+        *shared_var = 5;
+        std::cout << "Child process's shared_var after modification = " << *shared_var << "\n";
+    }
+    else
+    {
+        wait(NULL);
+        std::cout << "Parent process's shared_var = " << *shared_var << "\n";
+        *shared_var = 15;
+        std::cout << "Parent process's shared_var after modification = " << *shared_var << "\n";
+    }
+
+    munmap(shared_var, sizeof(int));
+    return 0;
+}
